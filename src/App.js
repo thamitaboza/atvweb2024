2083f9d492a3f6d06d3eb21ff19820ffb6cf54e0
Rafaelob32 <rafael32ufc@alu.ufc.br>
2024-03-05 20:09:47 -0300

    temperatura

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import MeusDados from "./components/atividade00/02MeusDados";
+/*import MeusDados from "./components/atividade00/02MeusDados";*/
+import Temperatura from "./components/atividade00/03Temperatura";
 
-function App() {
+/*function App() {
   return (
     <div>
       <h1>Meus Dados</h1>
       <MeusDados nome="Thamires Taboza da Silva" curso="Engenharia de Software" universidade="Universidade Federal do Ceará" />
     </div>
   );
+}*/
+function App() {
+  return (
+    <Temperatura/>
+  );
 }
+
 
 export default App;
